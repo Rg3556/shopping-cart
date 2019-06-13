39da80988f04c43a85c2a6459573727cdcd0851a
Rg3556 <50929687+Rg3556@users.noreply.github.com>
2019-06-13 10:37:11 -0400

    Setup  date and time with appropriate format

--- shopping_cart.py
+++ shopping_cart.py
@@ -7,6 +7,9 @@
 
 # from pprint import pprint
 
+import datetime
+
+
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
@@ -43,19 +46,20 @@ products = [
 
 ## Identifier / Input
 
-total_price = 0
+subtotal_price = 0
+selected_ids = []
+
 while True:
     
     selected_id = input("Please input a product identifier: ") #> String input
     if selected_id == "DONE":
         break
     else: 
-        matching_products = [p for p in products if str(p["id"]) ==  str(selected_id)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("..." + matching_product["name"] + " " + str(matching_product["price"] ))
-
-
+        # matching_products = [p for p in products if str(p["id"]) ==  str(selected_id)]
+        # matching_product = matching_products[0]
+        # total_price = total_price + matching_product["price"]
+        # print("..." + matching_product["name"] + " " + str(matching_product["price"] ))
+        selected_ids.append(selected_id)
 
 ## Receipt / Output
 
@@ -64,21 +68,17 @@ print("GREEN FOODS GROCERY")
 print("WWW.GREEN-FOODS-GROCERY.COM")
 print("---------------------------------")
 
-
-# print("CHECKOUT AT: " + 2019-06-06 11:31 AM
+today = datetime.date.today()
+now = datetime.datetime.now()
+print("CHECKOUT AT: " +  str(now.strftime("%Y-%m-%d ")) + str(now.strftime("%I:%M %p")))# + 2019-06-06 11:31 AM
 print("---------------------------------")
 print("SELECTED PRODUCTS: ")
 
-
-# def products_name(products):
-#     return products["name"]
-# 
-# for p in matching_product:
-#     price_usd = "${0:.2f}".format(p["price"])
-#     print(" ... " + p["name"] + " (" + str(price_usd) + ")")
+# print(selected_ids)
 
 
-print("TOTAL PRICE: " + str(total_price))
+
+
 
 # A grocery store name of your choice
 # A grocery store phone number and/or website URL and/or address of choice
